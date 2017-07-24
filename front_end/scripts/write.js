9e6d3a763597b57a5277e02e876122b1395bea1e
KimJangHyeon <rlawkdgus75@gmail.com>
2017-07-24 11:27:52 +0900

    write

---
+++ front_end/scripts/write.js
@@ -0,0 +1,12 @@
+/**
+ * Created by rlawk on 2017-07-23.
+ */
+src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"
+
+
+
+function pass_value(){
+    var getval=$('#write_board').contents().find('body').html();
+    document.getElementById("send_board").innerText=getval;
+    alert(document.getElementById("send_board"));
+}
