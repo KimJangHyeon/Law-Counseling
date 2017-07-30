902663d53db8b4358910a02247f036549dac0379
KimJangHyeon <rlawkdgus75@gmail.com>
2017-07-30 09:26:50 +0900

    길 찾기

--- front_end/scripts/board.js
+++ front_end/scripts/board.js
@@ -1,3 +1,14 @@
 /**
  * Created by rlawk on 2017-07-29.
  */
+
+
+// arr의 총 갯수가 23개라면  -->3개
+// index=  1;로시작
+// (index-1)*10+1 ~ index*10 까지
+
+var index;
+
+function initialize(){
+    index=
+}
---
+++ front_end/scripts/map.js
@@ -0,0 +1,19 @@
+/**
+ * Created by rlawk on 2017-07-30.
+ */
+
+
+function myMap(){
+    lat=37.5558429;
+    lng=127.04946289999998;
+    var myCenter=new google.maps.LatLng(lat, lng);
+    var mapCanvas=document.getElementById("map_block");
+    var mapOptions={center:myCenter, zoom:18};
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
+  var infowindow = new google.maps.InfoWindow({
+    content: "요기~!"
+  });
+  infowindow.open(map,marker);
+}
